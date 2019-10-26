3d8132a74c97c9a3d6733891731925c7e4957d25
valt123 <valttei@hotmail.fi>
2019-10-26 23:29:16 +0300

    Added double click to delete to list items

--- js/javascript.js
+++ js/javascript.js
@@ -1,30 +1,61 @@
+$('#input').focus();
 var list = [];
+//On submit click
 $("#submit").click(
 function(){
-  var toAdd = $('input[name=Test]').val();
-  list.push(toAdd + '\n');
-  //console.log(list)
-  listing();
+  var toAdd = $('#input').val();
+  //Only add to list if input not empty
+  if (toAdd != "")
+  {
+    list.push(toAdd + '\n');
+  }
+
+  //Focus back to input input and clear it after submit
+  $('#input').focus();
+  $('#input').focus(function() {
+    $(this).val('');
+  });
+
+  listing(); //Update list
 });
 
+//Submit if enter is pressed
+$("#input").keyup(function(event){
+    if(event.keyCode == 13){
+      $("#submit").click();
+    }
+});
+
+//Deletes element on double clicking
+$(document).on('dblclick','li', function(){
+  var id = $(this).attr("id");
+  list.splice(id, 1);
+  $(this).toggleClass('strike').remove();
+  listing(); //Update list after removal
+});
+
+//Makes the list that user sees
 function listing()
 {
   _list = []
-  list.forEach(function(item, index, array) {
-  _list.push('<li>' + item + '</li>');
+  list.forEach(function(item, index) {
+  //Adds <li tags to the list item
+  _list.push('<li id="' + index + '">' + item + '</li>');
   });
   $('.list').html(_list);
 }
 
+//On clicking save button
 $("#save").click(
 function(){
   var formattedList = "";
-  list.forEach(function(item, index, array) {
+
+  list.forEach(function(item) {
+    //Puts list items to a one string to get rid of commas
     formattedList += "- " + item;
   });
-  //console.log(formattedList)
 
+  //Saves list to a text file
   var blob = new Blob([formattedList], { type: "text/plain;charset=utf-8" });
   saveAs(blob, "todo.txt");
-
 });
